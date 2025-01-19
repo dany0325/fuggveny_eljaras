7f12e46292f6868d895f87ff63fc9ce14d9cc8fa
dany0325 <kissdani0309@gmail.com>
2025-01-19 19:49:56 +0100

    Kész az első feladat

---
+++ Eljaras_3.py
@@ -0,0 +1,10 @@
+def compare(num1, num2):
+    if num1 > num2:
+        print("Az első szám nagyobb!")
+    elif num2 > num1:
+        print("A második szám nagyobb!")
+    else:
+        print("A két szám egyenlő!")
+
+
+compare(12, 123123123)
